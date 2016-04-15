9c30fbb38315bde8d1dcc2f251ca2291ea34c73e
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-15 14:40:12 -0500

    6.2.1 highlow game is complete.  notes added code cleaned up

--- highlow.php
+++ highlow.php
@@ -1,14 +1,15 @@
 <?php
 
+// get a random number at set it equal to the variable $numberUserIsGuessing
 $numberUserIsGuessing = mt_rand( 1 , 100 );
-    echo $numberUserIsGuessing . "\n";
 
+// start the game by telling user what to do
 echo "Hey n00b, im thinking of a number between 1 and 100, guess what it is to earn some respect\n";
 
+// variable for users input is set to usersGuess variable
 $usersGuess = trim(fgets(STDIN));
-    echo $usersGuess . PHP_EOL;
 
-
+// do the first part of this do/while as long as it is lower or higher than random number
 do{    
     if ($usersGuess < $numberUserIsGuessing){
         echo "Wrong, guess HIGHER.\n";
@@ -17,5 +18,7 @@ do{
         echo "Wrong, guess LOWER.\n";
         $usersGuess = trim(fgets(STDIN));
     }
+// do the top part as long as the below statement is NOT TRUE! 
 } while ($numberUserIsGuessing != $usersGuess);
+// user finally guessed the correct number and won the game
 echo "GOOD GUESS EINSTIEN!  1 in 100 were pretty good odds though, just saying\n";
